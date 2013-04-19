91695b3c0c05f520134122032f7c4f11acd31d97
Ezcaflowne <ezcaflowne@fullsail.edu>
2013-04-18 18:32:43 -0700

    Added first files to Github

---
+++ Project2/Week2/Week2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Week2
+//
+//  Created by Willson Ayotte (NEW) on 4/18/13.
+//  Copyright (c) 2013 Willson Ayotte (NEW). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Project2/Week2/Week2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Week2
+//
+//  Created by Willson Ayotte (NEW) on 4/18/13.
+//  Copyright (c) 2013 Willson Ayotte (NEW). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
