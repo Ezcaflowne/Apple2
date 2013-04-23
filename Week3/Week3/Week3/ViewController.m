1afef1f9a86258cf4266adc5a898ccddc7977cae
Ezcaflowne <ezcaflowne@fullsail.edu>
2013-04-22 23:27:12 -0700

    Added Week 3 Project Files

---
+++ Week3/Week3/Week3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Week3
+//
+//  Created by Willson Ayotte (NEW) on 4/22/13.
+//  Copyright (c) 2013 Willson Ayotte (NEW). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Week3/Week3/Week3Tests/Week3Tests.h
@@ -0,0 +1,13 @@
+//
+//  Week3Tests.h
+//  Week3Tests
+//
+//  Created by Willson Ayotte (NEW) on 4/22/13.
+//  Copyright (c) 2013 Willson Ayotte (NEW). All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Week3Tests : SenTestCase
+
+@end
